fb1be6b80d2838478f2d86a632960d0736e1023a
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-11-27 18:29:04 +0530

    Update 1.arraySearching.cpp

--- Array/1.arraySearching.cpp
+++ Array/1.arraySearching.cpp
@@ -1,1 +1,20 @@
 
+#include<iostream>
+using namespace std;
+//Function to search element in an array
+int search(int arr[],int n,int x)
+{    
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            return i;
+        }
+    }
+
+};
+int main(){
+    int x=5;
+    int arr[]={1,2,0,7,2,9,3,1,5,201,2,65};
+    
+    cout<<" X element found at index :"<<search(arr,12,x);
+    return 0;
+}
