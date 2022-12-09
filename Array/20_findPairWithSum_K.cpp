767262a62e0dd0f8f1b17521518899253e5ad999
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-09 18:46:49 +0530

    Add files via upload

---
+++ Array/20_findPairWithSum_K.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void findPair(int arr[],int n,int k){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==k){
+               cout <<arr[i]<<"+"<<arr[j]<<"="<<k<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[9]={6,8,4,5,2,3,7,9,1};
+    findPair(arr,9,10);
+    
+    return 0;
+}
