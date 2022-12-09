86da2c208c9d9ad15006056b1eaca241046d7599
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-09 19:27:52 +0530

    Update 18.swapAlternate.cpp

--- Array/18.swapAlternate.cpp
+++ Array/18.swapAlternate.cpp
@@ -7,7 +7,7 @@ void printArray(int arr[],int n){
 }
 void swapAlternate(int arr[],int n){
     for(int i=0;i<n;i+=2){
-        swap(arr[i],arr[i+1]);
+       if(i+1<n) swap(arr[i],arr[i+1]);
     }
 }
 int main(){
