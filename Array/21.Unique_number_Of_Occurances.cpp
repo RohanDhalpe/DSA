e69f8d53869241649c5d25ac7a403c7dfa314d7b
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-18 00:48:14 +0530

    Update 21.Unique_number_Of_Occurances.cpp

--- Array/21.Unique_number_Of_Occurances.cpp
+++ Array/21.Unique_number_Of_Occurances.cpp
@@ -1,56 +1,53 @@
+ 
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr)
-    {
-          vector<int>ans;
-          int size=arr.size();
-          sort(arr.begin(),arr.end());
-          int i=0;
-          while(i<size)
-        {   int count=1;
-            
-            for(int j=i+1;j<size;j++)
-            {   if(arr[i]==arr[j]){
-                    count++;
-                }else{
-                    break;
-                }
-            }
-            ans.push_back(count);
-            i=i+count;
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(auto num:arr){
+            mp[num]++;
         }
-        size=ans.size();
-        sort(ans.begin(),ans.end());
-        for(int i=0;i<size-1;i++)
-        {
-            if(ans[i]==ans[i+1]){
-                return false;
-            }
+
+        unordered_set<int>set;
+        for(auto it:mp){
+            int freq = it.second;
+            set.insert(freq);
         }
-        return true;
+        
+       //if size of mp = set that means all have unique freq.
+        return set.size() == mp.size();
+        
     }
 };
-//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // class Solution {
 // public:
-//     bool uniqueOccurrences(vector<int>& arr) {
-//         unordered_map<int,int>mp;
-        
-// //         Store the frequecny.
-//         for(auto num:arr){
-//             mp[num]++;
+//     bool uniqueOccurrences(vector<int>& arr)
+//     {
+//           vector<int>ans;
+//           int size=arr.size();
+//           sort(arr.begin(),arr.end());
+//           int i=0;
+//           while(i<size)
+//         {   int count=1;
+            
+//             for(int j=i+1;j<size;j++)
+//             {   if(arr[i]==arr[j]){
+//                     count++;
+//                 }else{
+//                     break;
+//                 }
+//             }
+//             ans.push_back(count);
+//             i=i+count;
 //         }
-        
-// //         check the count and insert in unordered set.
-//         unordered_set<int>set;
-        
-//         for(auto it:mp){
-//             int freq = it.second;
-//             set.insert(freq);
+//         size=ans.size();
+//         sort(ans.begin(),ans.end());
+//         for(int i=0;i<size-1;i++)
+//         {
+//             if(ans[i]==ans[i+1]){
+//                 return false;
+//             }
 //         }
-        
-// //         if size of mp is same as set that means all have unique freq.
-//         return set.size() == mp.size();
-        
+//         return true;
 //     }
 // };
