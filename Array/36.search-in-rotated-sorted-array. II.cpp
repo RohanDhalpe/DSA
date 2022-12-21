449c3de7c064fe4f435dd43d78f88a276d86778d
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-21 16:05:42 +0530

    Create 36.search-in-rotated-sorted-array. II.cpp

---
+++ Array/36.search-in-rotated-sorted-array. II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==nums[mid]) return true;
+
+            if((nums[low]==nums[mid])  && (nums[high]==nums[mid])){ ///if it contain duplicates
+                low++;
+                high--;
+            }
+            else if(nums[mid]>=nums[low])
+            {
+                if((target<nums[mid]) && (target>=nums[low])){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+
+            }else{
+                 if((target>nums[mid]) && (target<=nums[high])){
+                     low=mid+1;
+                 }else{
+                     high=mid-1;
+                 }
+            }
+        }
+        return false;
+    }
+};
