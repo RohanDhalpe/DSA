d64601edddc99ad0385c2eace4f760e4e59aa994
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-25 19:29:39 +0530

    Update 41.ArrayIntersection.II.cpp

--- Array/41.ArrayIntersection.II.cpp
+++ Array/41.ArrayIntersection.II.cpp
@@ -1,17 +1,18 @@
+//Array Intersection in Which array containing multiple Duplicates
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
        //for storing multiple values
-       multiset<int>s;// [1,2,2,1]
+       multiset<int>s;
         for(auto val:nums1){
                 s.insert(val);
         }
-
-       for(auto it:nums2 ){
-            if(s.count(it)){
-                ans.push_back(it);
-                s.erase(s.find(it));
+        
+       for(int i=0;i<nums2.size();i++){
+            if(s.find(nums2[i])!=s.end()){
+                ans.push_back(nums2[i]);
+                s.erase(s.find(nums2[i]));
             }
         }
        return ans; 
