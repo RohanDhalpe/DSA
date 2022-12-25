ea899854516ef824a3acdfc628a25f811c06653f
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-25 18:50:45 +0530

    Update 28_3Sum(TripletsWthGivenSum).cpp

--- Array/28_3Sum(TripletsWthGivenSum).cpp
+++ Array/28_3Sum(TripletsWthGivenSum).cpp
@@ -4,11 +4,13 @@ public:
     {    vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
          int n=nums.size();
-         for(int i=0;i<n;i++){
+         for(int i=0;i<n;i++)
+         {
              int a=nums[i]; //a+b+c=0
              int t=-a;      //b+c=-a
              int s=i+1,e=n-1;
-             while(s<e){
+             while(s<e)
+             {
                  if(nums[s]+nums[e]==t)
                  {
                      ans.push_back({nums[i],nums[s],nums[e]});
@@ -18,7 +20,7 @@ public:
                      e--;
                  }else if(nums[s]+nums[e]>t)e--;
                   else s++;
-             }
+            }
              while( i+1<n && nums[i]==nums[i+1]) i++;
              //if two same element in nums[] array
          } 
