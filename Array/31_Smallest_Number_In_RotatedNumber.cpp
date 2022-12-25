b8cf9b3618eecd84eed7b143f40a4d0ed84dc655
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-25 18:54:35 +0530

    Update and rename 31_SmallestNumber_In_RotatedNumber.cpp to 31_Smallest_Number_In_RotatedNumber.cpp

--- Array/31_Smallest_Number_In_RotatedNumber.cpp
+++ Array/31_Smallest_Number_In_RotatedNumber.cpp
@@ -4,10 +4,11 @@ class Solution
     //Function to find the minimum element in sorted and rotated array.
     int minNumber(int arr[], int low, int high){
                                    //[ 4,5,6,7,0,1,2 ]
-        while(low<high){         //   [------][l    h]
+        while(low<high)
+        {         //   [------][l    h]
             int mid=(low+high)/2;
             if(arr[mid]>arr[high]){
-                low=mid+1;;
+                low=mid+1;
             }else {
                 high=mid;
             }
