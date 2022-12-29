cd51ab5020785234033db5f3a889347cfcc043b8
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2022-12-29 10:54:05 +0530

    Create 57_BestTime_Buy_Sell_Stock.cpp

---
+++ Array/57_BestTime_Buy_Sell_Stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice=INT_MAX;
+        int maxPro=0;
+
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }else if(prices[i]-minPrice>maxPro){
+                maxPro=prices[i]-minPrice;
+            }
+        }
+        return maxPro;
+    }
+};
