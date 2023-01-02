bcc09cbba44d6cff845d686fe5bac2f8be800a0d
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-02 12:53:41 +0530

    Create 61_ConcatenationOfArray.cpp

---
+++ Array/61_ConcatenationOfArray.cpp
@@ -0,0 +1,16 @@
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+           nums.push_back(nums[i]); 
+        }
+        return nums;
+    }
+};
