b7f61d5ce2989079d76ec39d9a50f1e52b9b4599
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-25 21:45:08 +0530

    Create 07_pat.cpp

---
+++ Must Do Pattern Questions/07_pat.cpp
@@ -0,0 +1,50 @@
+Input: 5
+
+Output:
+    *
+   ***  
+  *****
+ *******
+*********
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){  // for space              [n-i-1][2*i-1][n-i-1]
+                cout<<" ";
+            }
+            for(int j=0;j<2*i+1;j++){
+                cout<<"*";             //  for star
+            }
+             for(int j=0;j<n-i-1;j++){
+                cout<<" ";              // for space
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
