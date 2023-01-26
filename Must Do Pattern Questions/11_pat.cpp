f959459129277a26e134543ec94bc9eb17de5af0
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-26 09:58:18 +0530

    Create 11_pat.cpp

---
+++ Must Do Pattern Questions/11_pat.cpp
@@ -0,0 +1,46 @@
+Input: 5
+
+Output:
+1 
+0 1 
+1 0 1
+0 1 0 1 
+1 0 1 0 1
+  
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++)
+        {   int start=1;
+            if(i%2==0) start=1;
+            else start=0;
+            for(int j=0;j<=i;j++){
+                cout<<start<<" ";
+                start=1-start;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
