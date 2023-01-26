fcc813666ee465bd6efa82a1db78f03da044d426
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-26 10:44:41 +0530

    Create 16_pat.cpp

---
+++ Must Do Pattern Questions/16_pat.cpp
@@ -0,0 +1,44 @@
+Input: 5
+
+Output:
+A
+BB
+CCC
+DDDD
+EEEEE
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            char ch='A'+i;
+            for(int j=0;j<=i;j++){
+                cout<<ch;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
