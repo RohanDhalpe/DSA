bade569065408ae59c14b960128c5ed850bb9bf7
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-26 15:07:23 +0530

    Create 03_Armstrong.cpp

---
+++ Basic Maths/03_Armstrong.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        int sum=0;
+        int k=n;
+        while(n!=0){
+            int dig=n%10;
+            sum=sum+dig*dig*dig;
+            n=n/10;
+        }
+        if(sum==k)cout<<"Yes";
+        else cout<<"No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
