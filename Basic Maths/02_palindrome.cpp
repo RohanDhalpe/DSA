44ffd1b9f12ac8b2f9062bcf419bab05a4c41359
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-26 15:06:32 +0530

    Create 02_palindrome.cpp

---
+++ Basic Maths/02_palindrome.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    int k=n;
+		    int rev=0;
+		    while(n!=0){
+		         rev=rev*10+(n%10);
+		        n=n/10;
+		    }
+		    if(rev==k)cout<<"Yes";
+		    else cout<<"No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
