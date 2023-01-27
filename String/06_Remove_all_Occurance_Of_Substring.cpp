5421c97f23ffdda37f58c326a9a74d25367122da
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-27 22:45:30 +0530

    Create 06_Remove_all_Occurance_Of_Substring.cpp

---
+++ String/06_Remove_all_Occurance_Of_Substring.cpp
@@ -0,0 +1,27 @@
+Input: s = "daabcbaabcbc", part = "abc"
+Output: "dab"
+Explanation: The following operations are done:
+- s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+- s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+- s = "dababc", remove "abc" starting at index 3, so s = "dab".
+Now s has no occurrences of "abc".
+Example 2:
+
+Input: s = "axxxxyyyyb", part = "xy"
+Output: "ab"
+Explanation: The following operations are done:
+- s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
+- s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
+- s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
+- s = "axyb", remove "xy" starting at index 1 so s = "ab".
+Now s has no occurrences of "xy".
+ ---------------------------------------------------------------------------------------------------------------------------------------------- 
+  class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part)<=s.length()){
+           s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};
