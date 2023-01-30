d3280f029273ea968a1d90cb87e1136f3161f16e
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-30 16:17:44 +0530

    Create 03_SelectionSort.cpp

---
+++ Sorting Techniques/03_SelectionSort.cpp
@@ -0,0 +1,12 @@
+ void insertionSort(int arr[], int n)
+    {
+        for(int i=0;i<n;i++){
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
