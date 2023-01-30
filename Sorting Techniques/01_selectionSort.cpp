cfd91831ce2c78d913504ea518d2a757e455831e
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-01-30 15:54:08 +0530

    Create 01_selectionSort.cpp

---
+++ Sorting Techniques/01_selectionSort.cpp
@@ -0,0 +1,12 @@
+ void selectionSort(int arr[], int n)
+    {
+       for(int i=0;i<n-1;i++){
+           int minInd=i;
+           for(int j=i+1;j<n;j++){
+               if(arr[j]<arr[minInd]){
+                   minInd=j;
+               }
+           }
+           swap(arr[i],arr[minInd]);
+       }
+    }
