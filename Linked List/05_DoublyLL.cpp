394454940f4c78fb9fc7e1eadfec56118a5121ef
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-02-24 21:13:51 +0530

    Update 05_DoublyLL.cpp

--- Linked List/05_DoublyLL.cpp
+++ Linked List/05_DoublyLL.cpp
@@ -1,3 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                        GFG 
+ // Given an integer array arr of size n. Construct the doubly linked list from arr and return the head of it.
+
+// Example 1:
+
+// Input:
+// n = 5
+// arr = [1,2,3,4,5]
+// Output:
+// 1 2 3 4 5
+// Explanation: Linked list for the given array will be 1<->2<->3<->4<->5
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr) {
+        Node* previous=new Node(arr[0]);
+        Node* head=previous;
+        
+        for(int i=1;i<arr.size();i++){
+            Node* curr=new Node(arr[i]);
+            previous->next=curr;
+            curr->prev=previous;
+            previous=curr;
+        }
+      return head;
+    }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include<iostream>
 using namespace std;
 
