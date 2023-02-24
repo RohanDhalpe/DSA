0b4452ac839cc68fedd830fee59f829ad843ed59
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-02-24 13:32:49 +0530

    Create 08_Array_TO_LinkedList.cpp

---
+++ Linked List/08_Array_TO_LinkedList.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& nums) {
+        Node* head=NULL;
+        Node* tail=NULL;
+        
+        head=new Node(nums[0]);
+        
+        tail=head;
+        for(int i=1;i<nums.size();i++){
+            Node* temp=new Node(nums[i]);
+            tail->next=temp;
+            tail=tail->next;
+        }
+      return head;
+    }
+};
+
