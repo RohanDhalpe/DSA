13a486b80070039e3e44e8da5aae051e617a12b4
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-04-26 18:25:18 +0530

    Update 07_Height_Of_Tree.cpp

--- Tree/07_Height_Of_Tree.cpp
+++ Tree/07_Height_Of_Tree.cpp
@@ -1,25 +1,40 @@
-class Solution {
-public:
-    int maxDepth(TreeNode* root) 
-    {
-        if(root==NULL) return 0;
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) 
+//     {
+//         if(root==NULL) return 0;
         
-         queue<TreeNode*> q;
-         q.push(root);
-         int depth=0;
+//          queue<TreeNode*> q;
+//          q.push(root);
+//          int depth=0;
 
-         while(!q.empty())
-         {  
-             int size=q.size();
-             for(int i=0;i<size;i++)
-             { 
-                 TreeNode* node=q.front();
-                 q.pop();
-                 if(node->left)q.push(node->left);
-                 if(node->right)q.push(node->right);
-             }
-            depth++;
-         }
-      return depth;
+//          while(!q.empty())
+//          {  
+//              int size=q.size();
+//              for(int i=0;i<size;i++)
+//              { 
+//                  TreeNode* node=q.front();
+//                  q.pop();
+//                  if(node->left)q.push(node->left);
+//                  if(node->right)q.push(node->right);
+//              }
+//             depth++;
+//          }
+//       return depth;
+//     }
+// };
+
+
+class Solution{
+    public:
+    
+    int height(struct Node* node){
+        if(node==NULL){
+            return 0;
+        }
+        int lh= height(node->left);
+        int rh= height(node->right);
+        
+        return 1+max(lh,rh);
     }
 };
