98d7c2b21e7d8dacfeee21d2dc0ec147624ac8ac
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-04-26 17:51:16 +0530

    Create 01_Basic.cpp

---
+++ Tree/01_Basic.cpp
@@ -0,0 +1,37 @@
+Given an integer i. Print the maximum number of nodes on level i of a binary tree.
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+       return i==1 ? 1 :pow(2,i-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends	
