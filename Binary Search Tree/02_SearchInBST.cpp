6b1ca15de2ade03332ef436e475ed6dd54704780
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-05-03 01:04:25 +0530

    Create 02_SearchInBST.cpp

---
+++ Binary Search Tree/02_SearchInBST.cpp
@@ -0,0 +1,12 @@
+bool search(Node* root, int x) {
+   while(root!=NULL)
+   {
+      if(root->data==x) return true;
+      else if(root->data>x){
+          root=root->left;
+      }else{
+          root=root->right;
+      }
+   }
+   return false;
+}
